2fc55935819ed0225a7ddd1bf0cad4ab793a9154
63130131 <mihalamp@gmail.com>
2021-03-29 10:59:34 +0200

    Cleaning code

--- ContactForm.php
+++ ContactForm.php
@@ -8,24 +8,18 @@
     <style>
 
 
-      /* typography */
+      /* TYPOGRAPGY */
       *{font-family: Montserrat;font-size:15px;}
       a{color: #FD4F57; text-decoration: none;}
       a:hover{color:#e54b50;}
       h1{font-size: 5em;}
       h2{ font-size:2em; font-weight: 400; }
       h5{ font-size:14px; display: inline-block; color: #A0A0A0; font-weight: 400; }
+      .highlight{text-decoration: underline;text-transform: capitalize; text-decoration-color: #ffda49; text-decoration-thickness: 3px;  font-weight: 800;font-size:1em;}
 
-      .highlight{
-          text-decoration: underline;
-          text-transform: capitalize;
-          text-decoration-color: #ffda49;
-          text-decoration-thickness: 3px;
-          font-weight: 800;
-          font-size:1em;
-      } 
+       
 
-      /* outer box */
+      /* MAIN CONTAINER */
       .form-box{
         display:flex; 
         flex-wrap: wrap;
@@ -37,47 +31,55 @@
       .form-box > div{
         padding:3em;
       } 
-      
-      label{
-        text-transform: uppercase;
-        font-size:15px;
-        font-weight: 600;
-        color:#A0A0A0;
+
+
+      /* INPUT FIELDS */
+      .form-part{
+          display: flex;
+          position: relative;
+          flex-direction: column-reverse;
       }
 
-
-
-        input[type="text"], input[type="number"]{
+      input[type="text"], input[type="number"]{
           width:100%;
-          padding:1em 0;
+          /*padding:1em 0;*/
           border-top: none;
           border-left: none;
           border-right: none;
-          border-bottom: 1px solid #A0A0A0 !important;
+          border-bottom: 1px solid #A0A0A0;
           background: none !important;
           /*transition: all 0.2s ease;*/
+          padding: 14px 0 12px 0;
+          font-size: 30px !important;
           
         }
-        
+
        input[type="text"]:focus, input[type="number"]:focus{
           outline: 0;
-          border-bottom: 2px solid #FD4F57 !important;
+          border-bottom: 2px solid #FD4F57 ;
 
         }
 
-       /* input:focus + label{
-          font-size:12px !important;
+        /* LABELS */
+        label{
+          text-transform: uppercase;
+          font-weight: 600;
+          color:#A0A0A0;
+        }
+
+        /* label on focus */
+       input:focus + label{
+          font-size:10px;
           color: #FD4F57;
           transition: all 0.6s ease;
-        }  */
-
+        } 
+        /* label when input not empty  */
         .fokus{
-          font-size:12px !important;
-          color: #FD4F57;
-          transition: all 0.6s ease;
+          font-size:10px;
         }
 
-        /* button */
+
+        /* BUTTON */
         input[type="submit"]{
           border: 2px solid #fd4f57;
           color: #FD4F57;
@@ -93,12 +95,8 @@
           background: #fd4f57;
         } 
 
-      .form-part{
-          display: flex;
-          position: relative;
-          flex-direction: column-reverse;
-      }
-
+  
+        /* ALERTS */
        .errorMsg{
           position:absolute;
           right:0;
@@ -106,9 +104,11 @@
           font-size: 12px;
         }
 
-       #legalTerms{
+       #formMsg{
         padding-left:7px;
        }
+
+
        
     </style>
   </head>
@@ -124,24 +124,22 @@
           $restaurant = htmlspecialchars(stripslashes(trim($_POST['restaurant'])));
           $zip = htmlspecialchars(stripslashes(trim($_POST['zip'])));
 
-
-
           if(!preg_match("/^[A-Za-z .'-]+$/", $name)){
-            $errName = 'Invalid characters in name';
-          }
+            $errName = 'Invalid name.';
+          } 
           if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
             $errEmail = 'Invalid email';
           }
           if(strlen($phone) < 8){
-            $errPhone = 'Invalid phone';
+            $errPhone = 'Invalid phone.';
           }
          
           if(!preg_match("/^[A-Za-z .'-]+$/", $restaurant)){
-            $errRestaurant = 'Invalid characters';
+            $errRestaurant = 'Invalid characters.';
           }
          
           if(strlen((string)$zip) === 0){
-            $errZip = 'Zip code should be between 1000 and 9999';
+            $errZip = 'Zip code should be between 1000 and 9999.';
           }
         }
     ?>
@@ -161,38 +159,42 @@
         <div class="form">
           <form id="demoForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
                 <div class="form-part">
-                    <input type="text" name="name" onkeyup="changeTheColorOfButtonDemo();"> 
-                    <label for="name">Name</label>
+                    <input class="inputF" type="text" name="name"> 
+                    <label class="labelF" for="name">Name</label>
                     <span class="errorMsg"><?php if(isset($errName)) echo $errName; ?></span><br>
-                </div>
+                </div>    
                 <div class="form-part">
-                    <input id="test" type="text" name="email">
-                    <label class="testL" for="email" style="<?php if(isset($email)) echo "font-size:30px;"; ?>">Email</label>
+                    <input class="inputF" type="text" name="email">
+                    <label class="labelF" for="email" style="<?php /*if(isset($email)) echo "font-size:30px;";*/ ?>">Email</label>
                     <span class="errorMsg"><?php if(isset($errEmail)) echo $errEmail; ?></span><br>
                 </div>
                 <div class="form-part">
-                    <input type="text" name="phone" >
-                    <label for="phone"> Phone </label>
+                    <input class="inputF"type="text" name="phone" >
+                    <label class="labelF" for="phone"> Phone </label>
                     <span class="errorMsg"><?php if(isset($errPhone)) echo $errPhone; ?></span><br>
                 </div>
                 <div class="form-part">
-                    <input type="text" name="restaurant">
-                    <label for="restaurant">Restaurant Name</label>
+                    <input class="inputF" type="text" name="restaurant">
+                    <label class="labelF" for="restaurant">Restaurant Name</label>
                     <span class="errorMsg"><?php if(isset($errRestaurant)) echo $errRestaurant; ?></span><br>
                 </div>
                 <div class="form-part">
-                    <input type="number" id="quantity" name="zip" min="1000" max="9999">
-                    <label for="zip"> Restaurant Zip code</label>
+                    <input class="inputF" type="number" id="quantity" name="zip" min="1000" max="9999">
+                    <label class="labelF" for="zip"> Restaurant Zip code</label>
                     <span class="errorMsg"><?php if(isset($errZip)) echo $errZip; ?></span><br>
                 </div>
                 <br><br>
 
                 <input class="style-1" type="submit" name="submit" value="Submit">
-                <span id="legalTerms"><h5>By tapping submit, you concede to our Legal Terms.</h5></span>
+                <span id="formMsg"><h5>By tapping submit, you concede to our Legal Terms.</h5></span>
                 <?php 
                   if(isset($_POST['submit']) && !isset($errName) && !isset($errEmail) && !isset($errPhone) && !isset($errRestaurant) && !isset($errZip)){
+                    $body = $name . ' made a request ' . ' from restaurant ' . $restaurant;
+
                     if(mail('test@mail.com', $name, $body)){
-                      echo '<script>document.getElementById("legalTerms").innerHTML="<h5>Thank you! You will hear from us shortly!</h5>";</script>';
+                      /*echo '<script>document.getElementById("formMsg").innerHTML="<h5>Thank you! You will hear from us shortly!</h5>";</script>';*/
+                      echo '<script> $("#formMsg").html("<h5>Thank you! You will hear from us shortly!</h5>").fadeIn(500); $("#demoForm input").hide(); $("#demoForm .form-part").hide(); </script>';
+
                     }
                   }
                 ?>
@@ -205,30 +207,16 @@
           alert('Text1 changed!');
         }); */
 
-
         $(document).ready(function() {
-          $('#test').on('change', function() {
+          $('.inputF').on('change', function() {
             var $this = $(this);
-            var val = $.trim($this.val());
-
-            // toggleClass can be provided a bool value,
-            // If we provide true we add class, if false we remove class
-            $this.toggleClass('fokus', val.length !== 0);
+            var val = $.trim($(this).val());
+            /* when typing toggle class fokus */
+            $this.parent().find('.labelF').toggleClass('fokus', val.length !== 0);
           }).change();
-          // We also want to call a 'change' event on this just incase the page has
-          // pre-filled in values
         });
 
-      
-
-
-          /* 
-          if (document.getElementById("test").value !== "") {
-            document.getElementById("demoForm").style.background = "green";
-          } else {
-            document.getElementById("demoForm").style.background = "skyblue";
-          }
-        }*/
+    
     </script>
     </div>
   
